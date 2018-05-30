7df3ec7668139dd2bb96d19066ea9cbb54a5988c
Boris Peterbarg <boris@seekingalpha.com>
2018-05-30 16:34:13 +0300

    Tests: fix annoying mocking warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 require 'puppet-syntax/tasks/puppet-syntax'
 
 include RspecPuppetFacts
 
 RSpec.configure do |c|
+  c.mock_with :rspec
   c.after(:suite) do
     RSpec::Puppet::Coverage.report!
   end
   c.formatter = 'documentation'
 end
+
+require 'puppetlabs_spec_helper/module_spec_helper'
